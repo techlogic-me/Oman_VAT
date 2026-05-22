603d46fcd777bb73847c0dd4f834a7ae42956f8b
asif <asifc246@gmail.com>
2026-05-22 14:04:50 +0530

    update

--- oman_vat/events/barcode.py
+++ oman_vat/events/barcode.py
@@ -1,13 +1,13 @@
-# # your_app/utils.py
-# import barcode
-# from barcode.writer import SVGWriter
-# import io
-# import base64
+# your_app/utils.py
+import barcode
+from barcode.writer import SVGWriter
+import io
+import base64
 
-# def get_barcode_svg(value):
-#     # Using 'code128' for high-density, high-quality output
-#     code = barcode.get('code128', value, writer=SVGWriter())
-#     buffer = io.BytesIO()
-#     code.write(buffer)
-#     # Return as data URI string
-#     return f"data:image/svg+xml;base64,{base64.b64encode(buffer.getvalue()).decode('utf-8')}"
+def get_barcode_svg(value):
+    # Using 'code128' for high-density, high-quality output
+    code = barcode.get('code128', value, writer=SVGWriter())
+    buffer = io.BytesIO()
+    code.write(buffer)
+    # Return as data URI string
+    return f"data:image/svg+xml;base64,{base64.b64encode(buffer.getvalue()).decode('utf-8')}"
